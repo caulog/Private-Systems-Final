d68b6be6666a711994c1a28f4993b0a6dbff1623
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2025-04-16 16:49:24 -0400

    feat: use ibis w/ duckdb to read data into a table

--- find-quasi-ids/main.py
+++ find-quasi-ids/main.py
@@ -1,1 +1,18 @@
+import argparse
 
+import ibis
+
+
+def set_argparse():
+    parser = argparse.ArgumentParser(
+        prog="find-quasi-ids",
+    )
+    parser.add_argument("-f", "--file", required=True, help="Path to CSV datafile")
+    return parser
+
+
+if __name__ == "__main__":
+    parser = set_argparse()
+    args = parser.parse_args()
+
+    table = ibis.read_csv(args.file)
