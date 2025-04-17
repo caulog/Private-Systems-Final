19c63b9de5f677aa6c5353c516c81f1bd3263d84
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2025-04-17 14:27:45 -0400

    feat: run brute-force or greedy algo based on command line argument

--- find-quasi-ids/main.py
+++ find-quasi-ids/main.py
@@ -3,12 +3,21 @@ import argparse
 import ibis
 
 import brute_force
+import greedy
+
 
 def set_argparse():
     parser = argparse.ArgumentParser(
         prog="find-quasi-ids",
     )
     parser.add_argument("-f", "--file", required=True, help="Path to CSV datafile")
+    parser.add_argument(
+        "-a",
+        "--algo",
+        required=True,
+        choices=["brute", "greedy"],
+        help="Algorithm to run.",
+    )
     return parser
 
 
@@ -18,6 +27,10 @@ if __name__ == "__main__":
 
     table = ibis.read_csv(args.file)
 
-    # Run brute-force analysis
-    df = table.to_pandas()
-    brute_force.compute_distinct_ratios_by_size(df)
+    match args.algo:
+        case "brute":
+            df = table.to_pandas()
+            brute_force.compute_distinct_ratios_by_size(df)
+        case "greedy":
+            sampled_table = greedy.sample_for_greedy_min_key(table)
+            greedy.greedy_min_key(sampled_table)
