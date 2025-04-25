27761d276c06259ff0d5812688ecd5af833ae6e2
Pablo Ordorica Wiener <pablo_ordoricaw@hotmail.com>
2025-04-24 21:41:24 -0400

    Packaged Python CLI tool `find-quasi-ids` (#1)
    * feat: package into Python CLI tool
    
    * brute force commented
    
    * analysis of how many point to one person
    
    * Delete data/brute_force_output.txt
    
    * updated % metric
    
    * chore: mv find-quasi-ids Python CLI tool to its own dir
    
    * chore: ruff fmt
    
    * docs: init find-quasi-ids README
    
    * chore: Python dependencies with find-quasi-ids --editable installed
    
    * new data increased granularity
    
    * feat: package into Python CLI tool
    
    * chore: recreate uv.lock
    
    * chore: cleanup remaning files from old package structure
    
    * docs: find-quasi-ids overview, installation and start usage docs
    
    * docs: main README with project overview
    
    ---------
    
    Co-authored-by: caulog <caulog20@wfu.edu>
    Co-authored-by: Olivia Caulfield <77706580+caulog@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# [Privacy Study - Public Data] NY Tuition Dataset Project
+
+**Team:**
+
+- Olivia Caulfield
+- Pablo Ordorica Wiener
+
+**Course:** COMSE 6998 Privacy Systems
+
+- **Semester:** Spring 2025
+- **Instructor:** Roxana Geambasu
+
+- **TA:** Peihan Liu
+
+## Overview
+
+We investigated the [NYC Tuition Assitance Program data](https://data.ny.gov/Education/Tuition-Assistance-Program-TAP-Recipients-Dollars-/2t78-bs45/about_data) looking for quasi-identifiers that could lead to potential privacy leaks.
+
+As part of our study, we developed a Python CLI tool, `find-quasi-ids`. We hope this tool is useful in conducting other privacy-preserving data analysis.
--- find_quasi_ids/src/find_quasi_ids/brute_force.py
+++ find_quasi_ids/src/find_quasi_ids/brute_force.py
@@ -2,11 +2,14 @@ from itertools import combinations
 import pandas as pd
 import os
 
-def compute_distinct_ratios_by_size(df, attribute_min=6, attribute_max=6, top_k=10, output_dir="output"):
+
+def compute_distinct_ratios_by_size(
+    df, attribute_min=6, attribute_max=6, top_k=10, output_dir="output"
+):
     """
     Brute force analysis of data frame to find combinations of attributes with high distinct ratios
 
-    Distinct ratio definition from paper: 
+    Distinct ratio definition from paper:
     A combination is an α-distinct quasi-identifier if, after removing ≤ (1−α) tuples, the rest have unique values in that combination.
 
         To compute:
@@ -27,25 +30,33 @@ def compute_distinct_ratios_by_size(df, attribute_min=6, attribute_max=6, top_k=
         # loop through all possible combinations of columns
         # this is very expensive when r and m are large -- but our dataset is small
         for combo in combinations(m, r):
-            subset = df[list(combo)]                        # takes subset of dataframe with only current columns
-            num_unique = len(subset.drop_duplicates())      # removes duplicate rows to get unique row count
-            ratio = num_unique / n                          # calculates distinct ratio
-            
+            subset = df[
+                list(combo)
+            ]  # takes subset of dataframe with only current columns
+            num_unique = len(
+                subset.drop_duplicates()
+            )  # removes duplicate rows to get unique row count
+            ratio = num_unique / n  # calculates distinct ratio
+
             # adds each to result dataframe
-            results.append({
-                'attributes': ", ".join(combo),
-                'distinct_ratio': round(ratio, 4),
-                'unique_combinations': num_unique
-            })
+            results.append(
+                {
+                    "attributes": ", ".join(combo),
+                    "distinct_ratio": round(ratio, 4),
+                    "unique_combinations": num_unique,
+                }
+            )
 
         # sorts all combinations by highest distinct ratio and returns top k
-        results.sort(key=lambda x: -x['distinct_ratio'])
+        results.sort(key=lambda x: -x["distinct_ratio"])
         top_results = results[:top_k]
 
         # Print to console
         print(f"\nTop {top_k} combinations of size {r} by distinct ratio:\n")
         for r_entry in top_results:
-            print(f"{r_entry['attributes']}: distinct_ratio={r_entry['distinct_ratio']}, unique_combinations={r_entry['unique_combinations']}")
+            print(
+                f"{r_entry['attributes']}: distinct_ratio={r_entry['distinct_ratio']}, unique_combinations={r_entry['unique_combinations']}"
+            )
 
         # Save to CSV
         output_path = os.path.join(output_dir, f"brute_force_distinct_ratio_k{r}.csv")
